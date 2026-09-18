640e725104c8da3911d424f107378eef22cb79e1
Don <Don.rhodes@icloud.com>
2026-09-18 19:05:11 -0400

    added fx and sound

---
+++ Effects JSON.js
@@ -0,0 +1,91 @@
+Effects JSON
+
+Phaser
+
+{
+	"alignParticles": true,
+	"angle": -1,
+	"angleRandom": 0,
+	"duration": 3,
+	"emissionRate": 60,
+	"emitterSize": 0.5,
+	"endColour": [255, 0, 0, 1],
+	"endColourRandom": [60, 60, 60, 0],
+	"isPointtoPoint": true,
+	"lifeSpan": 15,
+	"lifeSpanRandom": 0,
+	"maxParticles": 300,
+	"rulerType": "beam",
+	"size": 10,
+	"sizeRandom": 0,
+	"speed": 40,
+	"speedRandom": 20,
+	"startColour": [255, 36, 0, 1],
+	"startColourRandom": [0, 0, 50, 0.25]
+}
+
+Photon
+
+
+{
+	"additionalEmissionRatePerPixel": 0.35,
+	"additionalSpeedPerPixel": 0.085,
+	"alignParticles": true,
+	"angle": -1,
+	"angleRandom": 0,
+	"duration": 12,
+	"emissionRate": 12,
+	"emitterSize": 4,
+	"lifeSpan": 13,
+	"lifeSpanRandom": 0,
+	"maxParticles": 50,
+	"rulerType": "line",
+	"size": 15,
+	"sizeRandom": 15,
+	"speed": 0,
+	"speedRandom": 0
+}
+
+
+Missile
+
+{
+	"angle": -1,
+	"angleRandom": 0,
+	"duration": 10,
+	"emissionRate": 500,
+	"emitterSize": 2,
+	"endColour": [154, 205, 50, 0.7],
+	"endColourRandom": [60, 60, 60, 0],
+	"isPointtoPoint": true,
+	"lifeSpan": 50,
+	"lifeSpanRandom": 3,
+	"maxParticles": 500,
+	"rulerType": "line",
+	"size": 10,
+	"sizeRandom": 1,
+	"speed": 100,
+	"speedRandom": 50,
+	"startColour": [0, 120, 0, 1],
+	"startColourRandom": [0, 0, 50, 0.25]
+}
+
+Solid
+{
+	"angle": -1,
+	"angleRandom": 0.2,
+	"duration": 20,
+	"emissionRate": 1,
+	"endColour": [175, 130, 50, 0],
+	"endColourRandom": [20, 20, 20, 0],
+	"gravity": {"x":0.01, "y":0.01},
+	"lifeSpan": 30,
+	"lifeSpanRandom": 1,
+	"maxParticles": 50,
+	"size": 10,
+	"sizeRandom": 1,
+	"speed": 50,
+	"speedRandom": 1.5,
+	"startColour": [175, 0, 0, 1],
+	"startColourRandom": [20, 0, 0, 0]
+}
